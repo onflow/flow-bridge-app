6e4c60f274f2497bb2f2738f2fff4aa568f78bf0
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2025-01-16 14:44:07 -0500

    fixed params

--- solana-oapp/layerzero.config.ts
+++ solana-oapp/layerzero.config.ts
@@ -3,6 +3,8 @@ import { ExecutorOptionType } from '@layerzerolabs/lz-v2-utilities'
 import layerZero from './config/layerzero.json'
 
 import type { OAppOmniGraphHardhat, OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
+import { ExecutorPDADeriver } from '@layerzerolabs/lz-solana-sdk-v2'
+import { PublicKey } from '@solana/web3.js'
 
 const flowMainnet = layerZero['EVM-on-Flow-Mainnet']
 const solanaMainnet = layerZero["Solana-Mainnet"]
@@ -17,6 +19,10 @@ const solanaContract: OmniPointHardhat = {
     eid: EndpointId.SOLANA_V2_MAINNET,
     address: '2KUb8dcZR9LyrSg4RdkQx91xX6mPQLpS1MEo6gwfvLZk', // NOTE: update this with the OFTStore address.
 }
+
+
+const executorPublicKey = new PublicKey(solanaMainnet.executor);
+const executorPDA = new ExecutorPDADeriver(executorPublicKey).config()
 
 const config: OAppOmniGraphHardhat = {
     contracts: [
@@ -49,14 +55,14 @@ const config: OAppOmniGraphHardhat = {
                         gas: 200000,
                         value: 200000,
                     },
-            //         {
-            //             // Solana options use (gas == compute units, value == lamports)
-            //             msgType: 2,
-            //             optionType: ExecutorOptionType.COMPOSE,
-            //             index: 0,
-            //             gas: 0,
-            //             value: 0,
-            //         },
+                    {
+                        // Solana options use (gas == compute units, value == lamports)
+                        msgType: 2,
+                        optionType: ExecutorOptionType.COMPOSE,
+                        index: 0,
+                        gas: 0,
+                        value: 0,
+                    },
                 ],
 
                 sendLibrary: flowMainnet.sendUln302,
@@ -90,52 +96,53 @@ const config: OAppOmniGraphHardhat = {
             // Having neither will cause a revert when calling send().
             // We suggest performing additional profiling to ensure they are correct for your use case.
             config: {
-                //     sendLibrary: '7a4WjyR8VZ7yZz5XJAKm39BUGn5iT9CKcv2pmG9tdXVH',
-                //     receiveLibraryConfig: {
-                //         receiveLibrary: '7a4WjyR8VZ7yZz5XJAKm39BUGn5iT9CKcv2pmG9tdXVH',
-                //         gracePeriod: BigInt(0),
-                //     },
-                //     // Optional Send Configuration
-                //     // @dev Controls how the `from` chain sends messages to the `to` chain.
-                //     sendConfig: {
-                //         executorConfig: {
-                //             maxMessageSize: 10000,
-                //             // The configured Executor address.  Note, this is the executor PDA not the program ID.
-                //             executor: 'AwrbHeCyniXaQhiJZkLhgWdUCteeWSGaSN1sTfLiY7xK',
-                //         },
-                //         ulnConfig: {
-                //             // // The number of block confirmations to wait before emitting the message from the source chain.
-                //             confirmations: BigInt(10),
-                //             // The address of the DVNs you will pay to verify a sent message on the source chain ).
-                //             // The destination tx will wait until ALL `requiredDVNs` verify the message.
-                //             requiredDVNs: [
-                //                 '4VDjp6XQaxoZf5RGwiPU9NR1EXSZn2TP4ATMmiSzLfhb', // LayerZero
-                //             ],
-                //             // The address of the DVNs you will pay to verify a sent message on the source chain ).
-                //             // The destination tx will wait until the configured threshold of `optionalDVNs` verify a message.
-                //             optionalDVNs: [],
-                //             // The number of `optionalDVNs` that need to successfully verify the message for it to be considered Verified.
-                //             optionalDVNThreshold: 0,
-                //         },
-                //     },
-                //     // Optional Receive Configuration
-                //     // @dev Controls how the `from` chain receives messages from the `to` chain.
-                //     receiveConfig: {
-                //         ulnConfig: {
-                //             // The number of block confirmations to expect from the `to` chain.
-                //             confirmations: BigInt(2),
-                //             // The address of the DVNs your `receiveConfig` expects to receive verifications from on the `from` chain ).
-                //             // The `from` chain's OApp will wait until the configured threshold of `requiredDVNs` verify the message.
-                //             requiredDVNs: [
-                //                 '4VDjp6XQaxoZf5RGwiPU9NR1EXSZn2TP4ATMmiSzLfhb', // LayerZero
-                //             ],
-                //             // The address of the DVNs you will pay to verify a sent message on the source chain ).
-                //             // The destination tx will wait until the configured threshold of `optionalDVNs` verify a message.
-                //             optionalDVNs: [],
-                //             // The number of `optionalDVNs` that need to successfully verify the message for it to be considered Verified.
-                //             optionalDVNThreshold: 0,
-                //         },
-                //     },
+                sendLibrary: solanaMainnet.sendUln302,
+                receiveLibraryConfig: {
+                    receiveLibrary: solanaMainnet.receiveUln302,
+                    gracePeriod: BigInt(10),
+                },
+                // Optional Send Configuration
+                // @dev Controls how the `from` chain sends messages to the `to` chain.
+                sendConfig: {
+                    executorConfig: {
+                        maxMessageSize: 10000,
+                        // The configured Executor address.  Note, this is the executor PDA not the program ID.
+                        // executor: 'AwrbHeCyniXaQhiJZkLhgWdUCteeWSGaSN1sTfLiY7xK',
+                        executor: executorPDA[0].toString(),
+                    },
+                    ulnConfig: {
+                        // // The number of block confirmations to wait before emitting the message from the source chain.
+                        confirmations: BigInt(0),
+                        // The address of the DVNs you will pay to verify a sent message on the source chain ).
+                        // The destination tx will wait until ALL `requiredDVNs` verify the message.
+                        requiredDVNs: [
+                            '4VDjp6XQaxoZf5RGwiPU9NR1EXSZn2TP4ATMmiSzLfhb', // LayerZero
+                        ],
+                        // The address of the DVNs you will pay to verify a sent message on the source chain ).
+                        // The destination tx will wait until the configured threshold of `optionalDVNs` verify a message.
+                        optionalDVNs: [],
+                        // The number of `optionalDVNs` that need to successfully verify the message for it to be considered Verified.
+                        optionalDVNThreshold: 0,
+                    },
+                },
+                // Optional Receive Configuration
+                // @dev Controls how the `from` chain receives messages from the `to` chain.
+                receiveConfig: {
+                    ulnConfig: {
+                        // The number of block confirmations to expect from the `to` chain.
+                        confirmations: BigInt(0),
+                        // The address of the DVNs your `receiveConfig` expects to receive verifications from on the `from` chain ).
+                        // The `from` chain's OApp will wait until the configured threshold of `requiredDVNs` verify the message.
+                        requiredDVNs: [
+                            '4VDjp6XQaxoZf5RGwiPU9NR1EXSZn2TP4ATMmiSzLfhb', // LayerZero
+                        ],
+                        // The address of the DVNs you will pay to verify a sent message on the source chain ).
+                        // The destination tx will wait until the configured threshold of `optionalDVNs` verify a message.
+                        optionalDVNs: [],
+                        // The number of `optionalDVNs` that need to successfully verify the message for it to be considered Verified.
+                        optionalDVNThreshold: 0,
+                    },
+                },
                 enforcedOptions: [
                     {
                         msgType: 1,
@@ -148,28 +155,6 @@ const config: OAppOmniGraphHardhat = {
                         gas: 200000,
                     },
                 ],
-
-                sendLibrary: solanaMainnet.sendUln302,
-                receiveLibraryConfig: {
-                    receiveLibrary: solanaMainnet.receiveUln302,
-                    gracePeriod: BigInt(0),
-                },
-                sendConfig: {
-                    executorConfig: {
-                        executor: solanaMainnet.executor,
-                        maxMessageSize: 10000,
-                    },
-                    ulnConfig: {
-                        confirmations: BigInt(0),
-                        requiredDVNs: ["4VDjp6XQaxoZf5RGwiPU9NR1EXSZn2TP4ATMmiSzLfhb"],
-                    },
-                },
-                receiveConfig: {
-                    ulnConfig: {
-                        confirmations: BigInt(0),
-                        requiredDVNs: ["4VDjp6XQaxoZf5RGwiPU9NR1EXSZn2TP4ATMmiSzLfhb"],
-                    },
-                },
             },
         },
     ],
