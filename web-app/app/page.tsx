651e7f8c454aba4e15e5c80d605a8c460914a36a
Tom Haile <bradford.haile@gmail.com>
2025-11-13 11:30:31 -0600

    add dark theme support

--- web-app/app/page.tsx
+++ web-app/app/page.tsx
@@ -8,7 +8,10 @@ export default function Home() {
       <nav className="top-nav">
         <div className="nav-content">
           <div className="logo">
-            <img src="/flow-logo.svg" alt="Flow Logo" width="100px" height="32px" />
+            <picture>
+              <source srcSet="/flow-logo-dark.svg" media="(prefers-color-scheme: dark)" />
+              <img src="/flow-logo.svg" alt="Flow Logo" width="100" height="42" />
+            </picture>
           </div>
         </div>
       </nav>
